4b85946fdaa4c046127423a7cc2f2df99fa6d7be
drangadrang0830 <boss0830@hotmail.com.tw>
2025-12-08 21:29:57 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+// 引入套件
+import { createPinia } from 'pinia'
+import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+//引入頁面
+import App from './App.vue'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueAxios, axios)
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { onMounted } from 'vue';
+const url = import.meta.env.VITE_APP_API
+const Path = import.meta.env.VITE_APP_PATH
+
+onMounted(() => { console.log(url, Path) })
+</script>
+
+<template>
+  <h2>當前 API 端點: <strong>{{ url || '未定義' }}</strong></h2>
+  <h2>當前 API 路徑: <strong>{{ Path || '未定義' }}</strong></h2>
+</template>
